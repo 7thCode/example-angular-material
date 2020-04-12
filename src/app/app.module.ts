25a070e55c7ea52124a032520d72dcc8cfcc9537
MIKIO ODA <oda.mikio@gmail.com>
2020-04-12 12:18:18 +0900

    start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,46 +1,50 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {NgModule} from "@angular/core";
+import {BrowserModule} from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule} from "@angular/forms";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {AppRoutingModule} from "./app-routing.module";
+import {AppComponent} from "./app.component";
 
-import {FormsModule} from '@angular/forms';
+import {MatButtonModule} from "@angular/material/button";
+import {MatCardModule} from "@angular/material/card";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatIconModule} from "@angular/material/icon";
+import {MatInputModule} from "@angular/material/input";
+import {MatMenuModule} from "@angular/material/menu";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatSnackBarModule} from "@angular/material/snack-bar";
+import {MatToolbarModule} from "@angular/material/toolbar";
 
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatButtonModule} from '@angular/material/button';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-
-import { AbcComponent } from './abc/abc.component';
-import { XyzComponent } from './xyz/xyz.component';
-import {MatCardModule} from '@angular/material/card';
-
+import {AbcComponent} from "./abc/abc.component";
+import {XyzComponent} from "./xyz/xyz.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    AbcComponent,
-    XyzComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatSidenavModule,
-    MatMenuModule,
-    MatButtonModule,
-    MatInputModule,
-    MatFormFieldModule,
-    FormsModule,
-    MatCardModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		AbcComponent,
+		XyzComponent,
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		BrowserAnimationsModule,
+		HttpClientModule,
+		MatToolbarModule,
+		MatIconModule,
+		MatSidenavModule,
+		MatMenuModule,
+		MatButtonModule,
+		MatInputModule,
+		MatFormFieldModule,
+		FormsModule,
+		MatCardModule,
+		MatSnackBarModule,
+	],
+	providers: [],
+	bootstrap: [AppComponent],
 })
-export class AppModule { }
+
+export class AppModule {
+}
